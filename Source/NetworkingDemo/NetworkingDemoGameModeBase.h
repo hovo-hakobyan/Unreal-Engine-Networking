19b1fc03e31a9a585a4a68ef0ea86244bc409f3c
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-07-05 15:53:12 +0200

    Initial commit

---
+++ Source/NetworkingDemo/NetworkingDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkingDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKINGDEMO_API ANetworkingDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
